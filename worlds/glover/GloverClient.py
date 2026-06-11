8afe4ec8aad9331ada2ef248dc6949871882b651
jjjj12212 <mjackson12212@hotmail.com>
2026-06-11 12:35:37 -0600

    Carnival 3 fix

--- worlds/glover/GloverClient.py
+++ worlds/glover/GloverClient.py
@@ -66,7 +66,7 @@ loc_name_to_id = network_data_package["games"]["Glover"]["location_name_to_id"]
 itm_name_to_id = network_data_package["games"]["Glover"]["item_name_to_id"]
 script_version: int = 1
 version: str = "V1.1.1"
-patch_md5: str = "5937cf9cc3dc04e94725d13477d09455"
+patch_md5: str = "86a4b830621767d674feb89f5eba14a5"
 gvr_options = settings.get_settings().glover_options
 program = None
 
